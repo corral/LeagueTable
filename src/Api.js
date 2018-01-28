b7dc12a7cbefef0aa0beeb39860bdbd072237c77
Toni Bardina <tonibardina@gmail.com>
2018-01-28 13:10:13 +0100

    first version

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const allInfo = axios.get('http://api.football-data.org/v1/competitions/455/leagueTable', {headers: {'X-Auth-Token': 'cab27f707e914b2fb5b9172fe2d3af0b'}})
+const allInfo = (number) => axios.get(`http://api.football-data.org/v1/competitions/455/leagueTable/?matchday=${number}`, {headers: {'X-Auth-Token': 'cab27f707e914b2fb5b9172fe2d3af0b'}})
 
 export { allInfo }
--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react'
 import { allInfo } from './Api'
 import './App.style.css'
 
+const uuidv1 = require('uuid/v1')
+
 class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      reverse: false
+      direction: 'down',
+      reverse: false,
+      matchday: 21,
+      matchdaySelected: 21,
     }
   }
 
-  componentWillMount () {
-    allInfo
+  componentWillMount() {
+    this.getTable()
+  }
+
+  getTable () {
+    allInfo(this.state.matchdaySelected)
     .then(info => {
       this.setState({
         standings: info.data.standing
       })
-      console.log(this.state)
     })
   }
 
@@ -24,46 +32,134 @@ class App extends Component {
     const { wins, draws, losses, goals, goalsAgainst} = place ? place : team
     const info = [ wins, draws, losses, goals, goalsAgainst]
     return info.map(info => {
-      return <span className='teamBoxElement'>{info}</span>
+      return <span key={uuidv1()} className='teamBoxElement'>{info}</span>
+    })
+  }
+
+  handleMatchDayClickLeft = () => {
+    this.setState({
+      matchdaySelected: this.state.matchdaySelected - 1,
+      reverse: this.state.standings[0].position === 1 && this.state.direction === 'down' ? false : true
+    })
+    this.getTable()
+  }
+
+  handleMatchDayClickRight = () => {
+    this.setState({
+      matchdaySelected: this.state.matchdaySelected + 1,
+      reverse: this.state.standings[0].position === 1 && this.state.direction === 'down' ? false : true
+    })
+    this.getTable()
+  }
+
+  handleClickDirection = () => {
+    this.setState({
+      direction: this.state.direction === 'down' ? 'up' : 'down',
+      reverse: true
     })
   }
 
   render () {
-    const standingsOrder = this.state.reverse ? this.state.standings.reverse() : this.state.standings
+    const standingsOrder = (
+      this.state.standings && this.state.reverse
+      ? this.state.standings.reverse()
+      : this.state.standings
+    )
     return (
       <div>
-        <ul>
+        <div className='header'>
+          <h1>Standings</h1>
+          <div>
+            <button onClick={this.handleMatchDayClickLeft}>
+              {'<'}
+            </button>
+            <h2>MATCHDAY {this.state.matchdaySelected}</h2>
+            { 
+              this.state.matchdaySelected < this.state.matchday 
+              ? <button onClick={this.handleMatchDayClickRight}>
+                  {'>'}
+                </button> 
+              : null
+            }
+          </div>
+        </div>
+        <div className='guide'>
+          <span>
+            {
+              this.state.direction === 'up'
+              ? <button onClick={this.handleClickDirection}>&uarr;</button>
+              : <button onClick={this.handleClickDirection}>&darr;</button>
+            }
+          </span>
+          <div>
+            <h3>Clips</h3>
+            <ul>
+              <li>PTS</li>
+              <li>Pld</li>
+              <li>W</li>
+              <li>D</li>
+              <li>L</li>
+              <li>GF</li>
+              <li>GA</li>
+            </ul>
+          </div>
+          <div>
+            <h3>At home</h3>
+            <ul>
+              <li>PTS</li>
+              <li>Pld</li>
+              <li>W</li>
+              <li>D</li>
+              <li>L</li>
+              <li>GF</li>
+              <li>GA</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Away</h3>
+            <ul>
+              <li>PTS</li>
+              <li>Pld</li>
+              <li>W</li>
+              <li>D</li>
+              <li>L</li>
+              <li>GF</li>
+              <li>GA</li>
+            </ul>
+          </div>
+        </div>
+        <ul className='table'>
           {
             this.state.standings
             ? standingsOrder.map(team => {
               return (
-                <li className='teamBox'>
-                  <div className='teamBoxDividerTeams'>
-                    <span className='teamPosition'>{team.position}</span>
-                    <span className='teamImage'>
-                      <img src={team.crestURI} alt='teamCrest' height='17' />
+                <li key={uuidv1()}>
+                  <div key={uuidv1()}>
+                    <span key={uuidv1()}>{team.position}</span>
+                    <span key={uuidv1()}>
+                      <img key={uuidv1()} src={team.crestURI} alt='teamCrest' height='17' />
                     </span>
-                    <span className='teamName'>{team.teamName}</span>
-                  </div>
-                  <span className='teamPoints'>{team.points}</span>
-                  <div className='teamBoxDividerStatistics'>
-                    <span className='teamBoxElement'>{team.playedGames}</span>
+                    <span key={uuidv1()}>{team.teamName}</span>
+                  </div> 
+                  <span key={uuidv1()}>{team.points}</span>
+                  <div key={uuidv1()}>
+                    <span key={uuidv1()} className='teamBoxElement'>{team.playedGames}</span>
                     {this.teamBoxElementCreator(team)}
                   </div>
-                  <span className='teamPoints'>{team.home.draws + (team.home.wins * 3)}</span>
-                  <div className='teamBoxDividerStatistics'>
-                    <span className='teamBoxElement'>{team.home.draws + team.home.losses + team.home.wins}</span>
+                  <span key={uuidv1()}>{team.home.draws + (team.home.wins * 3)}</span>
+                  <div key={uuidv1()}>
+                    <span key={uuidv1()} className='teamBoxElement'>{team.home.draws + team.home.losses + team.home.wins}</span>
                     {this.teamBoxElementCreator(team, team.home)}
                   </div>
-                  <span className='teamPoints'>{team.away.draws + (team.away.wins * 3)}</span>
-                  <div className='teamBoxDividerStatistics'>
-                    <span className='teamBoxElement'>{team.away.draws + team.away.losses + team.away.wins}</span>
+                  <span key={uuidv1()}>{team.away.draws + (team.away.wins * 3)}</span>
+                  <div key={uuidv1()}>
+                    <span key={uuidv1()} className='teamBoxElement'>{team.away.draws + team.away.losses + team.away.wins}</span>
                     {this.teamBoxElementCreator(team, team.away)}
                   </div>
                 </li>
               )
             })
-            : null
+            : <div className="loader"></div>
           }
         </ul>
       </div>
